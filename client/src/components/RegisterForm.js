e76b89c6777c5dea5f261ffd1f11f6c5f5dcf4eb
Maria Chira <chiramariageorgiana@gmail.com>
2022-03-10 23:43:47 +0000

    client register form completed

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,7 +1,11 @@
 import {
 	FormControl,
 	MenuItem,
-	TextField, Button, Container, Typography } from "@mui/material";
+	TextField,
+	Button,
+	Typography,
+	Container,
+} from "@mui/material";
 import styled from "@emotion/styled";
 import React from "react";
 
@@ -10,125 +14,232 @@ const roles = ["Admin", "Student"];
 const regions = ["London", "West Midlands", "Scotland", "Manchester"];
 const classes = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
 
+//Applying styles to elements
 const RegisterContainer = styled(Container)({
 	background: "#fafafa",
-    width: "max-content",
-    padding: "30px 50px",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
+	width: "max-content",
+	padding: "30px 50px",
+	display: "flex",
+	flexDirection: "column",
+	justifyContent: "center",
 });
+
 const RegisterButton = styled(Button)({
 	marginTop: "10px",
 });
 
+//Register Form Component
 const RegisterForm = () => {
-    //states for the options in the dropdown lists
-    const [role, setRole] =React.useState("");
-    const [region, setRegion] = React.useState("");
-    const [classNr, setClassNr] = React.useState("");
+	//states for storing the values of the inputs
+	const [formData, setFormData] = React.useState({
+		name: "",
+		email: "",
+		role: "",
+		region: "",
+		classNr: "",
+		password: "",
+		retypedPassword: "",
+	});
 
-    //events for updating the value of the selected options
-    const handleChangeRole = (event) => {
-        setRole(event.target.value);
-    };
-    const handleChangeRegion = (event) => {
-        setRegion(event.target.value);
-    };
-    const handleChangeClass = (event) => {
-        setClassNr(event.target.value);
-    };
+	//states for form inputs errors
+		const [nameError, setNameError] = React.useState(false);
+		const [emailError, setEmailError] = React.useState(false);
+		const [roleError, setRoleError] = React.useState(false);
+		const [regionError, setRegionError] = React.useState(false);
+		const [classNrError, setClassNrError] = React.useState(false);
+		const [passwordError, setPasswordError] = React.useState(false);
+		const [retypedPasswordError, setRetypedPasswordError] = React.useState(false);
 
-    return (
-			<RegisterContainer disableGutters={true}>
-				<Typography variant="h4" align="center">
-					Register
-				</Typography>
-				<FormControl margin="dense">
-					{/* field for name */}
-					<TextField id="register-name" label="Name" margin="dense" />
 
-					{/* field for email */}
-					<TextField
-						id="register-email"
-						label="Email"
-						type="email"
-						margin="dense"
-					/>
+	//functions to validate form inputs
+	const isNameValid = (name) => name.length < 5 ? false : true;
 
-					{/* dropdown to select the role */}
-					<TextField
-						id="register-role"
-						select
-						label="Role"
-						margin="dense"
-						value={role}
-						onChange={handleChangeRole}
-					>
-						{/* populate the roles dropdown with options */}
-						{roles.map((option, index) => (
-							<MenuItem key={index} value={option}>
-								{option}
-							</MenuItem>
-						))}
-					</TextField>
+	const isEmailValid = (email) => {
+		const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
+		return emailRegex.test(email) ? true : false;
+	};
 
-					{/* dropdown to select the region */}
-					<TextField
-						id="register-region"
-						select
-						label="Region"
-						margin="dense"
-						value={region}
-						onChange={handleChangeRegion}
-					>
-						{/* populate the regions dropdown with different options */}
-						{regions.map((option, index) => (
-							<MenuItem key={index} value={option}>
-								{option}
-							</MenuItem>
-						))}
-					</TextField>
+	const isSelectFieldValid = (value) => value.length < 1 ? false : true;
 
-					{/* dropdown to select the class */}
-					<TextField
-						id="register-class"
-						select
-						label="Class"
-						margin="dense"
-						value={classNr}
-						onChange={handleChangeClass}
-					>
-						{/* populate the classes dropdown with different options */}
-						{classes.map((option, index) => (
-							<MenuItem key={index} value={option}>
-								{option}
-							</MenuItem>
-						))}
-					</TextField>
+	const isPasswordValid = (password) => password.length < 5 ? false : true;
 
-					{/* Field for password */}
-					<TextField
-						id="register-password"
-						label="Password"
-						type="password"
-						margin="dense"
-					/>
+	const isRetypedPasswordValid = (retypedPassword) =>
+		formData.password == retypedPassword && retypedPassword.length > 5
+			? true
+			: false;
 
-					{/* Field for retyping password */}
-					<TextField
-						id="register-retype-password"
-						label="Retype Password"
-						type="password"
-						margin="dense"
-					/>
 
-					<RegisterButton variant="contained" size="medium">
-						Register
-					</RegisterButton>
-				</FormControl>
-			</RegisterContainer>
+	//calling all the validation functions in order to set the states for errors
+	//returning a boolean so I can do a general validation when the user try to submit the form
+	const areTheFieldsValid = () => {
+		isNameValid(formData.name) ? setNameError(false) : setNameError(true);
+		isEmailValid(formData.email) ? setEmailError(false) : setEmailError(true);
+		isSelectFieldValid(formData.role) ? setRoleError(false) : setRoleError(true);
+		isSelectFieldValid(formData.region) ? setRegionError(false) : setRegionError(true);
+		isSelectFieldValid(formData.classNr) ? setClassNrError(false) : setClassNrError(true);
+		isPasswordValid(formData.password) ? setPasswordError(false) : setPasswordError(true);
+		isRetypedPasswordValid(formData.retypedPassword) ? setRetypedPasswordError(false) : setRetypedPasswordError(true);
+
+		return (
+			isNameValid(formData.name) &&
+			isEmailValid(formData.email) &&
+			isSelectFieldValid(formData.role) &&
+			isSelectFieldValid(formData.region) &&
+			isSelectFieldValid(formData.classNr) &&
+			isPasswordValid(formData.password) &&
+			isRetypedPasswordValid(formData.retypedPassword)
 		);
+	};
+
+
+	//event for storing the input values in the formData state
+	const handleChangeInput = (event) => {
+		const objectKey = event.target.name;
+		const objectValue = event.target.value;
+		setFormData({ ...formData, [objectKey]: objectValue });
+	};
+
+	//submit form event
+	const handleSubmit = () => {
+
+		if (!areTheFieldsValid()) {
+			return;
+		} else {
+			fetch("http://localhost:3100/api/register", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(formData),
+			})
+				.then((res) => res.json())
+				.then((data) => console.log(data))
+				.catch((err) => console.log(err));
+		}
+	};
+
+	return (
+		<RegisterContainer disableGutters={true}>
+			<Typography variant="h4" align="center">
+				Register
+			</Typography>
+			<FormControl margin="dense">
+				{/* field for name */}
+				<TextField
+					id="name"
+					name="name"
+					label="Name"
+					margin="dense"
+					value={formData.name}
+					onChange={handleChangeInput}
+					error={nameError}
+					autoComplete="off"
+				/>
+
+				{/* field for email */}
+				<TextField
+					id="email"
+					name="email"
+					label="Email"
+					type="email"
+					margin="dense"
+					value={formData.email}
+					onChange={handleChangeInput}
+					error={emailError}
+					autoComplete="off"
+				/>
+
+				{/* dropdown to select the role */}
+				<TextField
+					id="role"
+					name="role"
+					select
+					label="Role"
+					margin="dense"
+					value={formData.role}
+					onChange={handleChangeInput}
+					error={roleError}
+				>
+					{/* populate the roles dropdown with options */}
+					{roles.map((option, index) => (
+						<MenuItem key={index} value={option}>
+							{option}
+						</MenuItem>
+					))}
+				</TextField>
+
+				{/* dropdown to select the region */}
+				<TextField
+					id="region"
+					name="region"
+					select
+					label="Region"
+					margin="dense"
+					value={formData.region}
+					onChange={handleChangeInput}
+					error={regionError}
+				>
+					{/* populate the regions dropdown with different options */}
+					{regions.map((option, index) => (
+						<MenuItem key={index} value={option}>
+							{option}
+						</MenuItem>
+					))}
+				</TextField>
+
+				{/* dropdown to select the class */}
+				<TextField
+					id="class"
+					name="classNr"
+					select
+					label="Class"
+					defaultValue=""
+					margin="dense"
+					value={formData.classNr}
+					onChange={handleChangeInput}
+					error={classNrError}
+				>
+					{/* populate the classes dropdown with different options */}
+					{classes.map((option, index) => (
+						<MenuItem key={index} value={option}>
+							{option}
+						</MenuItem>
+					))}
+				</TextField>
+
+				{/* Field for password */}
+				<TextField
+					id="password"
+					name="password"
+					label="Password"
+					type="password"
+					margin="dense"
+					value={formData.password}
+					onChange={handleChangeInput}
+					error={passwordError}
+				/>
+
+				{/* Field for retyping password */}
+				<TextField
+					id="retype-password"
+					name="retypedPassword"
+					label="Retype Password"
+					type="password"
+					margin="dense"
+					value={formData.retypedPassword}
+					onChange={handleChangeInput}
+					error={retypedPasswordError}
+				/>
+
+				<RegisterButton
+					type="submit"
+					variant="contained"
+					size="medium"
+					onClick={handleSubmit}
+				>
+					Register
+				</RegisterButton>
+			</FormControl>
+		</RegisterContainer>
+	);
 };
 
 export default RegisterForm;
