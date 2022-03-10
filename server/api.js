bf73ad103014cabe46eee3870e53ef92b0733112
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2022-03-10 18:12:41 +0000

    added get users with dietary requirements
    also reordered the endpoints a little

--- server/api.js
+++ server/api.js
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { response, Router } from "express";
 import query from "./db";
 
 const router = Router();
@@ -13,12 +13,29 @@ router.get("/", (_, res) => {
 	res.json({ message: "Hello, world!" });
 });
 
-//retrieve all users
-router.get("/users", (_, res) => {
+//get all users with allergies
+router.get("/users/allergies", (_, res) => {
 	client
-		.query("SELECT * FROM users")
+		.query(
+			`
+			SELECT u.user_name AS name, u.user_email AS email, a.allergy_name AS dietary_restriction
+			FROM dietary_restrictions AS d_r
+			INNER JOIN users AS u
+			ON u.id = d_r.user_id
+			INNER JOIN allergies AS a
+			ON a.id = d_r.allergen_id
+			`
+		)
 		.then((response) => {
-			res.send(response.rows);
+			//check students with dietary restrictions exist
+			console.log(response);
+			if (response.rowCount < 1) {
+				//no students with dietary restrictions found
+				res.status(400).send("No students with dietary restrictions found.");
+			} else {
+				//found students with dietary restrictions
+				res.send(response.rows);
+			}
 		})
 		.catch((error) => {
 			console.error(error);
@@ -30,7 +47,14 @@ router.get("/users", (_, res) => {
 router.get("/users/:userId", (req, res) => {
 	const id = req.params.userId;
 	client
-		.query("SELECT * FROM users WHERE id=$1",[id])
+		.query(
+			`
+			SELECT * 
+			FROM users 
+			WHERE id=$1
+			`,
+			[id]
+		)
 		.then((response) => {
 			//check if user was found:
 			if(response.rowCount < 1){
@@ -40,6 +64,19 @@ router.get("/users/:userId", (req, res) => {
 				//user found
 				res.send(response.rows);
 			}
+		})
+		.catch((error) => {
+			console.error(error);
+			res.status(error.status).send(error);
+		});
+});
+
+//retrieve all users
+router.get("/users", (_, res) => {
+	client
+		.query("SELECT * FROM users")
+		.then((response) => {
+			res.send(response.rows);
 		})
 		.catch((error) => {
 			console.error(error);
