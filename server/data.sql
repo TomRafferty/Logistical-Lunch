1ad71a653f1c09ebecfb2a0eaff14a65163a0005
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2022-03-10 18:21:59 +0000

    added some test data for dietary restrictions

--- server/data.sql
+++ server/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO cohort (class_number,region) VALUES (3, 'West Midlands');
 INSERT INTO users (user_name,user_email,is_admin,is_lunch_maker,is_lunch_shopper,user_location,cohort_id,user_password) VALUES ('john doe', 'johndoe@mail.com', false, false, false, 'birmingham',1,'password');
+
+-- dietary test:
+-- I didn't run the file here to avoid duplicate data but this is what I added through CLI
+INSERT INTO users (user_name,user_email,is_admin,is_lunch_maker,is_lunch_shopper,user_location,cohort_id,user_password) VALUES ('sue doe', 'suedoe@mail.com', false, false, false, 'birmingham',1,'password');
+INSERT INTO allergies (allergy_name) VALUES ('Some Dietary Requirement');
+INSERT INTO dietary_restrictions (user_id, allergen_id) VALUES(2, 1);
