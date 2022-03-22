750b209d3c103ef8e6c9c1e4b7c8ef6c6191ca2d
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2022-03-22 14:34:49 +0000

    basic layout and date picker set up
    no actual functionality yet.

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import Student from "./pages/Student";
 import LoginPage from "./pages/LoginPage";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import CreateEventForm from "./components/CreateEventForm";
 
 const App = () => (
 	<div>
 		<NavBar />
+		<CreateEventForm />
 		<Routes>
 			<Route path="/" element={<LoginPage />} />
 			<Route path="/student" element={<Student />} />
